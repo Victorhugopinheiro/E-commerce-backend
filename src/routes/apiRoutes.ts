e930291ac589362bb929438a36f88f12fe8337bf
Victor Hugo Pinheiro Lima <azullvictorhugo10@gmail.com>
2026-07-08 20:19:40 -0300

    new api route

--- src/routes/apiRoutes.ts
+++ src/routes/apiRoutes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import WebhookController from "../controllers/api/webhookController";
+import AsaasWebhookController from "../controllers/api/asaasWebhookController";
 import { authMiddleware } from "../middleware/adminMiddlewere";
 import { authCartMiddleware } from "../middleware/authCart";
 import bodyParser from "body-parser";
@@ -10,6 +11,6 @@ const router = Router();
 
 
 router.post("/call", authCartMiddleware, bodyParser.raw({type: 'application/json'}), new WebhookController().handle);  
-
+router.post("/asaasWebhook", bodyParser.raw({type: 'application/json'}), new AsaasWebhookController().handle);
 
 export default router;
