3641335be61a30e311bee70f7ef2504160dde67a
Victor Hugo Pinheiro Lima <azullvictorhugo10@gmail.com>
2026-07-08 20:17:33 -0300

    improving the logic of cpf controller

--- src/controllers/user/validate-cpf-controller.ts
+++ src/controllers/user/validate-cpf-controller.ts
@@ -4,13 +4,13 @@ import ValidateCpfService from "../../services/user/validate-cpf-service";
 class ValidateCpfController {
     async handle(req: Request, res: Response) {
         try {
-            const { userCpf, firstName,  lastName } = req.body;
+            const { userCpf, firstName, lastName } = req.body;
             const userId = req.body.userId;
             if (!userCpf) {
                 return res.status(400).json({ success: false, message: 'CPF é obrigatório.' });
             }
 
-          
+
             const cpfRegex = /^\d{11}$/;
             if (!cpfRegex.test(userCpf)) {
                 return res.status(400).json({ success: false, message: 'CPF deve conter exatamente 11 dígitos numéricos.' });
@@ -19,7 +19,7 @@ class ValidateCpfController {
             const serviceValidateCpf = new ValidateCpfService()
             const result = await serviceValidateCpf.execute({ userCpf, firstName, lastName, userId });
 
-           return res.json(result).status(result.success ? 200 : 400);
+            return res.json(result).status(result.success ? 200 : 400);
 
         } catch (error) {
             console.error('Erro ao validar CPF:', error);
